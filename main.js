540e1724cec69d6893aaa89e33ce852789561f4d
Wissenss <wissenschaftecht@gmail.com>
2022-08-07 10:56:43 -0500

    added total amount

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 //MODEL
 let records = [];
 
-const storage = JSON.parse(localStorage.getItem("expensesRecords"));
+window.onload = init;
 
-if(storage){
-    records=storage;
+//initialization function
+function init() {
+    const storage = JSON.parse(localStorage.getItem("expensesRecords"));
+
+    if (storage) {
+        records = storage;
+    }
+
+    renderList();
 }
 
-function saveRecords(){
+function saveRecords() {
     localStorage.setItem("expensesRecords", JSON.stringify(records));
 }
 
 //add record to database
-function addRecord(name, cost){
+function addRecord(name, cost) {
     const recordId = new Date().getTime() + ''
-    
+
     const record = {
-        name : name,
-        cost : cost,
+        name: name,
+        cost: cost,
         id: recordId
     }
 
@@ -26,11 +33,11 @@ function addRecord(name, cost){
 }
 
 //remove record from database
-function removeRecord(recordIdToRemove){
-    records = records.filter(record =>{
-        if(record.id === recordIdToRemove){
+function removeRecord(recordIdToRemove) {
+    records = records.filter(record => {
+        if (record.id === recordIdToRemove) {
             return false;
-        } else{
+        } else {
             return true;
         }
     });
@@ -38,11 +45,13 @@ function removeRecord(recordIdToRemove){
 }
 //VIEW
 //render list with current records[] information
-function renderList(){
+function renderList() {
     const list = document.getElementById("list");
     list.innerHTML = '';
+    let total = 0;
 
-    records.forEach(record =>{
+    records.forEach(record => {
+        total += parseFloat(record.cost);
         console.log("this runed");
         //create new div html element
         const newItem = document.createElement("article");
@@ -51,12 +60,12 @@ function renderList(){
 
         newItem.appendChild(nameLabel);
         newItem.classList.add("item");
-        
+
 
         //set record info in new html element
         nameLabel.innerText = record.name;
         nameLabel.style = "text-align: left; width:60%; display: inline-block;";
-        costLabel.innerText = record.cost;
+        costLabel.innerText = "$" + record.cost;
         costLabel.style = "text-align: right; width:25%; margin-right: 5%; display: inline-block;";
 
         //add delete button to new html element
@@ -72,22 +81,45 @@ function renderList(){
         //add new html element to document
         list.appendChild(newItem);
     });
+
+    //total amount field
+    if (records.length) {
+        const nameLabel = document.createElement("label");
+        nameLabel.style = "text-align: left; width:60%; display: inline-block;";
+        const costLabel = document.createElement("label");
+        costLabel.style = "text-align: right; width:25%; margin-right: 5%; display: inline-block;";
+        const newItem = document.createElement("article");
+        newItem.classList.add("item");
+        newItem.style = "border-top: 0px"
+
+        nameLabel.innerText = "Total amount:";
+        costLabel.innerText = total;
+
+        newItem.appendChild(nameLabel);
+        newItem.appendChild(costLabel);
+
+        const result = document.getElementById("total");
+        result.innerHTML = "";
+        result.appendChild(newItem);
+    }
 }
 
-renderList();
 //CONTROLLER
-function addExpenseButton(){
+function addExpenseButton() {
     const nameInput = document.getElementById("name-input");
     const costInput = document.getElementById("cost-input");
 
     const name = nameInput.value;
     const cost = costInput.value
 
+    nameInput.value = "";
+    costInput.value = "";
+
     addRecord(name, cost);
     renderList();
 }
 
-function removeExpenseButton(event){
+function removeExpenseButton(event) {
     const deleteButton = event.target;
     const recordIdToRemove = deleteButton.id;
 
